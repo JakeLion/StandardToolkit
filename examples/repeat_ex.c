23f797180a8450f1e1ba6563be53f78b45a44c23
JakeLion <69877521+JakeLion@users.noreply.github.com>
2022-06-21 20:59:21 -0600

    Create repeat_ex.c

---
+++ examples/repeat_ex.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "standard-toolkit/stdtoolkit.h"
+
+
+//example of the circuit feature
+int circuit_ex()
+{
+    circuit("Circuit Example", 5);
+    return 0;
+}
+
+//example of the loop feature
+int loop_ex()
+{
+    loop("Loop Example");
+    return 0;
+}
